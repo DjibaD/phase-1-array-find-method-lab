c334f3620b426a2df5ceb3b6a06b7aa9cf3cc48d
djibadiawara <djiba.diawara97@gmail.com>
2021-11-24 15:05:53 -0500

    commit tree cee1560b6bd7547924ee1502779879492f88ddef

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+  
+function superbowlWin(record) {
+    let winRecord = record.find (record => record.result ==="W")
+    if (winRecord) {
+        return winRecord.year
+    } else {
+    return undefined
+    }
+}
+
+
 
